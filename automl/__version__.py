14853c2e77e43dd1b5a25c3803c3a98fbec4e5ab
Nanda Rizkika Ruanawijaya <61659@bni.co.id>
2025-07-21 15:00:50 +0700

    Update code formatting and configuration

--- automl/__version__.py
+++ automl/__version__.py
@@ -3,4 +3,8 @@
 __version__ = "0.1.0"
 __author__ = "Nanda Rizkika Ruanawijaya"
 __email__ = "nandarizky52@gmail.com"
-__description__ = "A comprehensive, modular framework for automated machine learning with overfitting detection and mitigation"
+__description__ = (
+    "A comprehensive, modular framework for "
+    "automated machine learning with overfitting "
+    "detection and mitigation"
+)
