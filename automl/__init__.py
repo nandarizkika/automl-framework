6631115be4cf71a863529f01857c73b533ba7284
Nanda Rizkika Ruanawijaya <61659@bni.co.id>
2025-07-21 15:26:59 +0700

    Format code with Black and update files

--- automl/__init__.py
+++ automl/__init__.py
@@ -8,21 +8,16 @@ from .automl import AutoML
 from .hyperparameter_tuner import HyperparameterTuner
 from .automl_integration import TuningIntegrator
 
-from .__version__ import (
-    __version__,
-    __author__,
-    __email__,
-    __description__
-)
+from .__version__ import __version__, __author__, __email__, __description__
 
 __all__ = [
-    'AutoML', 
-    'DataUtils', 
-    'setup_logging', 
-    'HyperparameterTuner', 
-    'TuningIntegrator',
-    '__version__',
-    '__author__',
-    '__email__',
-    '__description__'
+    "AutoML",
+    "DataUtils",
+    "setup_logging",
+    "HyperparameterTuner",
+    "TuningIntegrator",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__description__",
 ]
